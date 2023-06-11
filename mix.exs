a828e93478bed0716c9687a9a4e666fae3ffea42
Michael Ruoss <michael@michaelruoss.ch>
2023-06-11 21:08:01 +0200

    changelog

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Kompost.MixProject do
   def project do
     [
       app: :kompost,
-      version: "0.1.2",
+      version: "0.2.2",
       elixir: "~> 1.14",
       start_permanent: Mix.env() == :prod,
       deps: deps(),
