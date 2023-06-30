c2d1f2933b8c286bf902dab20c1a0d92d94f445a
Michael Ruoss <michael@michaelruoss.ch>
2023-06-30 09:42:22 +0200

    Update test_helper.exs

--- test/test_helper.exs
+++ test/test_helper.exs
@@ -1,3 +1,3 @@
 DotenvParser.load_file("test/integration/.env")
 Application.ensure_all_started([:k8s, :postgrex, :db_connection])
-ExUnit.start(exclude: [:integration, :skip])
+ExUnit.start(exclude: [:integration, :e2e, :skip])
