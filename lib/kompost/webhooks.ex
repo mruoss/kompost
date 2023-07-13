092f409e4a4d118cba064bc61c1e88ff38644455
Michael Ruoss <michael@michaelruoss.ch>
2023-07-13 12:36:23 +0200

    pass secret name as variable

--- lib/kompost/webhooks.ex
+++ lib/kompost/webhooks.ex
@@ -5,14 +5,14 @@ defmodule Kompost.Webhooks do
 
   require Logger
 
-  @spec bootstrap_tls(atom()) :: :ok
-  def bootstrap_tls(env) do
+  @spec bootstrap_tls(atom(), binary()) :: :ok
+  def bootstrap_tls(env, secret_name) do
     Application.ensure_all_started(:k8s)
     conn = K8sConn.get!(env)
 
     with {:certs, {:ok, ca_bundle}} <-
            {:certs,
-            K8sWebhoox.ensure_certificates(conn, "kompost", "kompost", "kompost", "tls-certs")},
+            K8sWebhoox.ensure_certificates(conn, "kompost", "kompost", "kompost", secret_name)},
          {:webhook_config, :ok} <-
            {:webhook_config,
             K8sWebhoox.update_admission_webhook_configs(conn, "kompost", ca_bundle)} do
